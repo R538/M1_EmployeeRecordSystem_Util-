407e49339386e43abc26fcc90469fc23714239ba
R538 <ravishekharsingh53@gmail.com>
2022-02-11 11:52:36 +0530

    commit

---
+++ 3_Implementation/inc/main.h
@@ -0,0 +1,19 @@
+/**
+ * @file main.h
+ * @author ravishekhar.singh@ltts.com
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include<stdio.h>
+#include<string.h>
+#include "function.c"
+#include <stdlib.h>
+
+#endif
